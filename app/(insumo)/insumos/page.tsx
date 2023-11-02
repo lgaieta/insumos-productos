8a4eef9c659a06e3763c814232eead0298074ab3
lgaieta <lgaieta.dev@gmail.com>
2023-11-02 18:29:47 -0300

    Added responsive to input and button in insumos

--- app/(insumo)/insumos/page.tsx
+++ app/(insumo)/insumos/page.tsx
@@ -2,13 +2,12 @@ import { Input } from '@nextui-org/input';
 import { Button } from '@nextui-org/button';
 import Link from 'next/link';
 import MaterialsTable from './components/MaterialsTable';
-import MaterialsTableBodySkeleton from './components/MaterialsTableBodySkeleton';
 
 function MaterialsPage() {
     return (
         <main className='flex flex-col items-center px-8 py-12 w-full text-foreground-900 bg-background'>
             <h1 className='text-4xl font-bold mb-12 text-center'>Insumos</h1>
-            <div className='flex items-center justify-between w-full'>
+            <div className='flex flex-col-reverse gap-6 sm:flex-row items-center justify-between w-full'>
                 <Input
                     size='sm'
                     radius='md'
@@ -16,12 +15,13 @@ function MaterialsPage() {
                     variant='bordered'
                     isClearable
                     label='Buscar'
-                    classNames={{ base: 'max-w-[300px]' }}
+                    classNames={{ base: 'w-full sm:w-auto sm:max-w-[300px]' }}
                 />
                 <Button
                     color='primary'
                     as={Link}
                     href='/crear-insumo'
+                    className='w-full sm:w-fit'
                 >
                     Nuevo Insumo
                 </Button>
