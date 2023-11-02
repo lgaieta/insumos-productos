923064414ececf793da2de13b58cfbc62e91a838
lgaieta <lgaieta.dev@gmail.com>
2023-11-02 18:55:21 -0300

    Added more margin top to root loading file

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from '@nextui-org/spinner';
 
 function Loading() {
     return (
-        <div className='flex flex-col mt-10 gap-4 w-full items-center jusitfy-center'>
+        <div className='flex flex-col mt-12 gap-4 w-full items-center jusitfy-center'>
             <Spinner size='lg' />
             <p className='font-bold text-center'>Cargando página...</p>
         </div>
