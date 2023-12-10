3efed887ff8f1e48f552479fb496da8f77215572
lgaieta <lgaieta.dev@gmail.com>
2023-12-09 21:41:43 -0300

    Added prevent remove material image when editing

--- app/insumo/[id]/services/updateMaterialFromDatabase.ts
+++ app/insumo/[id]/services/updateMaterialFromDatabase.ts
@@ -4,5 +4,10 @@ import { pool } from '@common/services/pool';
 export const updateMaterialFromDatabase = async (material: DBMaterialAsOutput) => {
     const { INSUMO_ID, ...newMaterial } = material;
 
-    return await pool.query('UPDATE INSUMO SET ? WHERE INSUMO_ID = ?', [newMaterial, INSUMO_ID]);
+    const { IMAGEN, ...materialWithoutImage } = newMaterial;
+
+    return await pool.query('UPDATE INSUMO SET ? WHERE INSUMO_ID = ?', [
+        IMAGEN ? newMaterial : materialWithoutImage,
+        INSUMO_ID,
+    ]);
 };
