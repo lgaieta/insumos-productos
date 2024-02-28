3eec8c77f8c87ac343160b3c62676062dcfa3691
lgaieta <lgaieta.dev@gmail.com>
2024-02-28 17:15:31 -0300

    Fixed pagination error in api route when searching

--- app/insumos/api/route.ts
+++ app/insumos/api/route.ts
@@ -15,15 +15,21 @@ export async function GET(request: NextRequest) {
         ? parseInt(process.env.NEXT_PUBLIC_MATERIAL_ROWS_PER_PAGE)
         : 5;
 
+    const rowsStartIndex = (page - 1) * rowsPerPage;
+    const filterTextForQuery = `%${filterText || ''}%`;
+
     const data = (
         await pool.query<RowDataPacket[]>(
             'SELECT INSUMO_ID, NOMBRE, COSTO_UNITARIO, LINK FROM insumo WHERE NOMBRE LIKE ? LIMIT ?, ?',
-            [`%${filterText || ''}%`, (page - 1) * rowsPerPage, rowsPerPage],
+            [filterTextForQuery, rowsStartIndex, rowsPerPage],
         )
     )[0] as DBMaterial[];
 
     const rowCountQuery = (
-        await pool.query<RowDataPacket[]>('SELECT COUNT(*) AS total FROM insumo')
+        await pool.query<RowDataPacket[]>(
+            'SELECT COUNT(*) AS total FROM insumo WHERE NOMBRE LIKE ?',
+            [filterTextForQuery],
+        )
     )[0][0];
 
     return Response.json({ data: materialListAdapter(data), total: rowCountQuery.total });
