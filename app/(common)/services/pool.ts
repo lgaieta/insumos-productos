3cf43897a603501b51302edfef19ccc1ed68eb88
lgaieta <lgaieta.dev@gmail.com>
2024-03-02 19:43:36 -0300

    Added DB_PORT environment variable in pool

--- app/(common)/services/pool.ts
+++ app/(common)/services/pool.ts
@@ -4,5 +4,6 @@ export const pool = mysql.createPool({
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
     user: process.env.DB_USER,
+    port: process.env.DB_PORT,
     database: process.env.DB_DBNAME,
 });
