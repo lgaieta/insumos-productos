4de9736ac8d9e7dde74b10317fb9d8b36c1dfdcb
lgaieta <lgaieta.dev@gmail.com>
2024-03-07 14:46:11 -0300

    Added icons to home page

--- app/(home)/components/NavigationCard.tsx
+++ app/(home)/components/NavigationCard.tsx
@@ -1,20 +1,19 @@
 import { Card, CardBody, CardHeader } from '@nextui-org/card';
-import { Image } from '@nextui-org/image';
-import NextImage from 'next/image';
+import { ReactNode } from 'react';
 
 type NavigationCardProps = {
     title: string;
     subtitle: string;
-    imageProps: { src: string; alt: string; width: string; height: string };
+    bodyContent: ReactNode;
 };
 
 function NavigationCard(props: NavigationCardProps) {
     return (
         <Card
             classNames={{
-                base: 'px-8 py-4 border border-transparent lg:max-h-[50vh] transition-colors hover:border-foreground-300',
+                base: 'p-4 aspect-square border border-transparent lg:max-h-[50vh] transition-colors hover:border-primary-400',
                 header: 'flex flex-col gap-2',
-                body: 'justify-end p-0 py-4',
+                body: 'justify-end p-0 md:py-4',
             }}
             shadow='md'
             isPressable
@@ -24,21 +23,9 @@ function NavigationCard(props: NavigationCardProps) {
                 <h2 className='text-2xl font-bold w-full text-center text-foreground-800'>
                     {props.title}
                 </h2>
-                <p className='text-base text-foreground-400 w-full text-center'>
-                    {props.subtitle}
-                </p>
+                <p className='text-base text-foreground-400 w-full text-center'>{props.subtitle}</p>
             </CardHeader>
-            <CardBody>
-                {/* eslint-disable-next-line jsx-a11y/alt-text */}
-                <Image
-                    as={NextImage}
-                    {...props.imageProps}
-                    classNames={{
-                        wrapper: 'w-full justify-self-end aspect-[16 / 12]',
-                        img: 'h-full w-full object-cover',
-                    }}
-                />
-            </CardBody>
+            <CardBody>{props.bodyContent}</CardBody>
         </Card>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,58 @@
 import NavigationCard from '@/(home)/components/NavigationCard';
 import Link from 'next/link';
+import { TbBoxSeam } from 'react-icons/tb';
+import { TbShirt } from 'react-icons/tb';
+import { MdOutlineAddShoppingCart } from 'react-icons/md';
+import { TbPigMoney } from 'react-icons/tb';
 
 export default function Home() {
     return (
         <main className='flex flex-col items-center px-8 py-12 w-full text-foreground-900 bg-background'>
             <h1 className='text-4xl font-bold mb-12 text-center'>Costos y Precios</h1>
-            <nav className='grid grid-cols-1 gap-8 max-w-[600px] w-full sm:grid-cols-2 lg:max-w-[800px]'>
+            <nav className='grid grid-cols-1 gap-8 max-w-[280px] sm:max-w-[600px] w-full sm:grid-cols-2 lg:max-w-[800px]'>
                 <Link href='/insumos'>
                     <NavigationCard
                         title='Insumos'
                         subtitle='Costos de materiales'
-                        imageProps={{
-                            src: '/image3.png',
-                            alt: 'Insumos para elaborar productos',
-                            width: '250',
-                            height: '100',
-                        }}
+                        bodyContent={
+                            <TbBoxSeam
+                                size='100%'
+                                color='#212121'
+                            />
+                        }
                     />
                 </Link>
                 <NavigationCard
                     title='Productos'
                     subtitle='Artículos listos para vender'
-                    imageProps={{
-                        src: '/image3.png',
-                        alt: 'Productos listos para vender',
-                        width: '250',
-                        height: '100',
-                    }}
+                    bodyContent={
+                        <TbShirt
+                            size='100%'
+                            color='#212121'
+                        />
+                    }
                 />
                 <NavigationCard
                     title='Compras'
                     subtitle='Gastos en insumos'
-                    imageProps={{
-                        src: '/image3.png',
-                        alt: 'Insumos para elaborar productos',
-                        width: '250',
-                        height: '100',
-                    }}
+                    bodyContent={
+                        <MdOutlineAddShoppingCart
+                            className='p-4'
+                            size='100%'
+                            color='#212121'
+                        />
+                    }
                 />
                 <NavigationCard
                     title='Ventas'
                     subtitle='Transacciones completadas'
-                    imageProps={{
-                        src: '/image3.png',
-                        alt: 'Insumos para elaborar productos',
-                        width: '250',
-                        height: '100',
-                    }}
+                    bodyContent={
+                        <TbPigMoney
+                            className='p-4'
+                            size='100%'
+                            color='#212121'
+                        />
+                    }
                 />
             </nav>
         </main>
