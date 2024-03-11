d9d0b27e6245408ac221bae090fb32cd379b380a
lgaieta <lgaieta.dev@gmail.com>
2024-03-11 09:04:27 -0300

    Added nextjs routing inside NextUIProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,12 +2,15 @@
 
 import { NextUIProvider } from '@nextui-org/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
+    const router = useRouter();
+
     return (
-        <NextUIProvider>
+        <NextUIProvider navigate={router.push}>
             <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
         </NextUIProvider>
     );
