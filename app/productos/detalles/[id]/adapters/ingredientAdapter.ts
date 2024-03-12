628bae89ae56fa2cb50f29fa1a7ec0bf2921a709
lgaieta <lgaieta.dev@gmail.com>
2024-03-12 16:12:44 -0300

    Added ingredientAdapter

---
+++ app/productos/detalles/[id]/adapters/ingredientAdapter.ts
@@ -0,0 +1,15 @@
+import Ingredient, { IngredientType } from '@common/entities/Ingredient';
+import { DBIngredient } from '../services/getIngredientsFromDatabaseById';
+
+export const ingredientAdapter = (incomingIngredient: DBIngredient): Ingredient<IngredientType> => {
+    return {
+        id: incomingIngredient.FORMULA_DETALLE_ID,
+        productId: incomingIngredient.PRODUCTO_ID,
+        componentId: incomingIngredient.INSUMO_ID ?? incomingIngredient.SUBPRODUCTO_ID!,
+        type: incomingIngredient.INSUMO_ID !== null ? 'material' : 'product',
+        amount: parseFloat(incomingIngredient.CANTIDAD),
+    };
+};
+
+export const ingredientsListAdapter = (ingredientsList: DBIngredient[]) =>
+    ingredientsList.map(ingredientAdapter);
