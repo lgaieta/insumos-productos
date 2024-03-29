fcaa637b6851d44ff0aff89b0942168cf5324912
lgaieta <lgaieta.dev@gmail.com>
2024-03-29 18:15:40 -0300

    Added FetchParams to fetchMaterialImageList

--- app/insumos/services/fetchMaterialImageList.ts
+++ app/insumos/services/fetchMaterialImageList.ts
@@ -1,9 +1,8 @@
+import { FetchParams } from '@common/services/FetchTypes';
 import { addParamsToURL } from '@common/utils/addParamsToURL';
 import { MaterialImageListApiResponse } from '@insumos/api/imagenes/route';
 
-export const fetchMaterialImageList = async (
-    options?: RequestInit & { params: Record<string, string> },
-) => {
+export const fetchMaterialImageList = async (options?: FetchParams) => {
     const url = addParamsToURL('/insumos/api/imagenes', options?.params || {});
     const res = await fetch(url, options);
     const json: MaterialImageListApiResponse = await res.json();
