99a77d3601c6068e5f2b9f04d83d1bd5f425ab6d
lgaieta <lgaieta.dev@gmail.com>
2024-04-09 16:59:11 -0300

    Added path revalidating and loading status on ingredient deleting

--- app/productos/actions/deleteIngredientServerAction.ts
+++ app/productos/actions/deleteIngredientServerAction.ts
@@ -1,10 +1,17 @@
 'use server';
 
 import Ingredient from '@common/entities/Ingredient';
+import Product from '@common/entities/Product';
 import { deleteIngredientFromDatabase } from '@productos/services/deleteIngredientFromDatabase';
+import { revalidatePath } from 'next/cache';
 
-export async function deleteIngredientServerAction(ingredientId: Ingredient['id']) {
+export async function deleteIngredientServerAction(
+    ingredientId: Ingredient['id'],
+    productId: Product['id'],
+) {
     await deleteIngredientFromDatabase(ingredientId);
+
+    revalidatePath(`/productos/detalles/${productId}`);
 
     console.log(`Deleted ingredient ${ingredientId} successfully`);
 }
--- app/productos/detalles/[id]/components/ingredients-list/DeleteIngredientButton.tsx
+++ app/productos/detalles/[id]/components/ingredients-list/DeleteIngredientButton.tsx
@@ -1,25 +1,45 @@
 import Ingredient from '@common/entities/Ingredient';
+import Product from '@common/entities/Product';
 import { Button } from '@nextui-org/button';
+import { Spinner } from '@nextui-org/react';
 import { deleteIngredientServerAction } from '@productos/actions/deleteIngredientServerAction';
+import { usePathname } from 'next/navigation';
+import { useFormStatus } from 'react-dom';
 import { IoMdClose } from 'react-icons/io';
 
-function DeleteIngredientButton(props: { ingredientId: Ingredient['id'] }) {
-    const action = deleteIngredientServerAction.bind(null, props.ingredientId);
+function DeleteButton() {
+    const { pending } = useFormStatus();
 
     return (
-        <form action={action}>
-            <Button
-                variant='light'
-                className='data-[hover=true]:bg-default-300'
-                radius='full'
-                isIconOnly
-                type='submit'
-            >
+        <Button
+            variant='light'
+            className='data-[hover=true]:bg-default-300'
+            radius='full'
+            isIconOnly
+            type='submit'
+            isDisabled={pending}
+        >
+            {pending ? (
+                <Spinner classNames={{ wrapper: 'w-6 h-6' }} />
+            ) : (
                 <IoMdClose
                     size={24}
                     color='#11181C'
                 />
-            </Button>
+            )}
+        </Button>
+    );
+}
+
+function DeleteIngredientButton(props: { ingredientId: Ingredient['id'] }) {
+    const pathname = usePathname();
+    const productId: Product['id'] = parseInt(pathname.split('/').slice(-1)[0]);
+
+    const action = async () => await deleteIngredientServerAction(props.ingredientId, productId);
+
+    return (
+        <form action={action}>
+            <DeleteButton />
         </form>
     );
 }
