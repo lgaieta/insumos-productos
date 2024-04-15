1a928fc59a9c3e8f1fcb3944b0fb2ef08ea24676
lgaieta <lgaieta.dev@gmail.com>
2024-04-15 15:36:31 -0300

    Updated IngredientsListSkeleton

--- app/productos/detalles/[id]/components/ingredients-list/IngredientsListSkeleton.tsx
+++ app/productos/detalles/[id]/components/ingredients-list/IngredientsListSkeleton.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import { Card, CardHeader, CardBody } from '@nextui-org/card';
 import { Divider } from '@nextui-org/divider';
+import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell } from '@nextui-org/table';
+import { Button } from '@nextui-org/button';
 import { Skeleton } from '@nextui-org/skeleton';
 
 function IngredientsListSkeleton() {
@@ -7,43 +11,153 @@ function IngredientsListSkeleton() {
         <Card classNames={{ header: 'justify-between p-5', body: 'p-5' }}>
             <CardHeader>
                 <h2 className='text-xl font-bold'>Ingredientes</h2>
+                <Skeleton className='rounded-xl'>
+                    <Button>Añadir ingrediente</Button>
+                </Skeleton>
             </CardHeader>
             <Divider />
             <CardBody>
-                <ul className='flex flex-col'>
-                    <li className='flex flex-col gap-3 w-full items-start justify-center px-4 py-3'>
-                        <Skeleton className='rounded-full'>
-                            <p className='text-base leading-none'>Impresion color</p>
-                        </Skeleton>
-                        <Skeleton className='rounded-full'>
-                            <p className='text-tiny leading-none'>Insumo</p>
-                        </Skeleton>
-                    </li>
-                    <li className='flex flex-col gap-3 w-full items-start justify-center px-4 py-3'>
-                        <Skeleton className='rounded-full'>
-                            <p className='text-base leading-none'>Impresion color</p>
-                        </Skeleton>
-                        <Skeleton className='rounded-full'>
-                            <p className='text-tiny leading-none'>Insumo</p>
-                        </Skeleton>
-                    </li>
-                    <li className='flex flex-col gap-3 w-full items-start justify-center px-4 py-3'>
-                        <Skeleton className='rounded-full'>
-                            <p className='text-base leading-none'>Impresion color</p>
-                        </Skeleton>
-                        <Skeleton className='rounded-full'>
-                            <p className='text-tiny leading-none'>Insumo</p>
-                        </Skeleton>
-                    </li>
-                    <li className='flex flex-col gap-3 w-full items-start justify-center px-4 py-3'>
-                        <Skeleton className='rounded-full'>
-                            <p className='text-base leading-none'>Impresion color</p>
-                        </Skeleton>
-                        <Skeleton className='rounded-full'>
-                            <p className='text-tiny leading-none'>Insumo</p>
-                        </Skeleton>
-                    </li>
-                </ul>
+                <Table
+                    aria-label='Tabla de ingredientes pertenecientes al producto'
+                    removeWrapper
+                    isStriped
+                >
+                    <TableHeader>
+                        <TableColumn>
+                            <Skeleton className='rounded-full'>Nombre</Skeleton>
+                        </TableColumn>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>
+                                <div className='flex flex-col justify-center gap-2 items-start min-w-[200px]'>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-small !leading-[16px]'>
+                                            Nombre de insumo largo
+                                        </p>
+                                    </Skeleton>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-tiny text-foreground-400 !leading-none'>
+                                            Insumo
+                                        </p>
+                                    </Skeleton>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <div className='flex flex-col justify-center gap-2 items-start min-w-[200px]'>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-small !leading-[16px]'>
+                                            Nombre de insumo largo
+                                        </p>
+                                    </Skeleton>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-tiny text-foreground-400 !leading-none'>
+                                            Insumo
+                                        </p>
+                                    </Skeleton>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <div className='flex flex-col justify-center gap-2 items-start min-w-[200px]'>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-small !leading-[16px]'>
+                                            Nombre de insumo largo
+                                        </p>
+                                    </Skeleton>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-tiny text-foreground-400 !leading-none'>
+                                            Insumo
+                                        </p>
+                                    </Skeleton>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <div className='flex flex-col justify-center gap-2 items-start min-w-[200px]'>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-small !leading-[16px]'>
+                                            Nombre de insumo largo
+                                        </p>
+                                    </Skeleton>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-tiny text-foreground-400 !leading-none'>
+                                            Insumo
+                                        </p>
+                                    </Skeleton>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <div className='flex flex-col justify-center gap-2 items-start min-w-[200px]'>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-small !leading-[16px]'>
+                                            Nombre de insumo largo
+                                        </p>
+                                    </Skeleton>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-tiny text-foreground-400 !leading-none'>
+                                            Insumo
+                                        </p>
+                                    </Skeleton>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <div className='flex flex-col justify-center gap-2 items-start min-w-[200px]'>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-small !leading-[16px]'>
+                                            Nombre de insumo largo
+                                        </p>
+                                    </Skeleton>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-tiny text-foreground-400 !leading-none'>
+                                            Insumo
+                                        </p>
+                                    </Skeleton>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <div className='flex flex-col justify-center gap-2 items-start min-w-[200px]'>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-small !leading-[16px]'>
+                                            Nombre de insumo largo
+                                        </p>
+                                    </Skeleton>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-tiny text-foreground-400 !leading-none'>
+                                            Insumo
+                                        </p>
+                                    </Skeleton>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <div className='flex flex-col justify-center gap-2 items-start min-w-[200px]'>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-small !leading-[16px]'>
+                                            Nombre de insumo largo
+                                        </p>
+                                    </Skeleton>
+                                    <Skeleton className='rounded-full'>
+                                        <p className='text-tiny text-foreground-400 !leading-none'>
+                                            Insumo
+                                        </p>
+                                    </Skeleton>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
             </CardBody>
         </Card>
     );
